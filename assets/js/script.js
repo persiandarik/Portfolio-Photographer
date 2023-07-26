68feacbdad5bc7f791b7575c046613dac22bc07b
persiandarik <persiandarik@gmail.com>
2023-07-26 17:02:33 +0330

    'preloading'

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+
+
+// add Event on multiple elment
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+
+
+// PRELOADING
+
+const loadingElement = document.querySelector("[data-loading]");
+
+window.addEventListener("load", function () {
+  loadingElement.classList.add("loaded");
+  document.body.classList.remove("active");
+});
+
