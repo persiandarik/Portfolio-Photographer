3a3e2daf95002538d685df15cdaf4a45d0244cd5
persiandarik <persiandarik@gmail.com>
2023-08-03 17:54:55 +0330

    'back-top-btn'

--- assets/js/script.js
+++ assets/js/script.js
@@ -151,3 +151,27 @@ window.addEventListener("load", setLetterEffect);
 
 
 
+/**
+ * BACK TO TOP BUTTON
+ */
+
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  const bodyHeight = document.body.scrollHeight;
+  const windowHeight = window.innerHeight;
+  const scrollEndPos = bodyHeight - windowHeight;
+  const totalScrollPercent = (window.scrollY / scrollEndPos) * 100;
+
+  backTopBtn.textContent = `${totalScrollPercent.toFixed(0)}%`;
+
+  // visible back top btn when scrolled 5% of the page
+  if (totalScrollPercent > 5) {
+    backTopBtn.classList.add("show");
+  } else {
+    backTopBtn.classList.remove("show");
+  }
+});
+
+
+
